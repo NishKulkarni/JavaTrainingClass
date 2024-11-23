a97b0fc549962da25a5ba6b9be88a863be26d755
NishKulkarni <nishikant1243@gmail.com>
2024-11-23 21:03:21 +0530

    Commitingfromlocal

--- pack1/Test3.java
+++ pack1/Test3.java
@@ -1,5 +1,6 @@
 class Test3{
 	public static void main(String[] args){
-		System.out.println("Welcome to Java Sessions"); 
+		System.out.println("Welcome to Java Sessions");
+		System.out.println("Commiting from local to Remote");
 	}
 }
